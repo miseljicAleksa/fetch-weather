8894c296709ce8bdf1053b8ce9c509956a2ff5ae
Milan Velebit <velebit@accusys.co>
2019-08-14 11:07:16 +0200

    hotfix

--- src/components/form.js
+++ src/components/form.js
@@ -5,7 +5,7 @@ const Form = (props) => {
     return(
         <form onSubmit = {props.loadWeather}>
             <input type="text" name="city" placeholder="City" />
-            <input type="text" name="conutry" placeholder="Country"/>
+            <input type="text" name="country" placeholder="Country"/>
             <button>Get weather</button>
         </form>
     )
